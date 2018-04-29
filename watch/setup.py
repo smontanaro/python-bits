5a0f9051ced74ef19262c503c6b9604a6da6e3c2
Skip Montanaro <skip.montanaro@gmail.com>
2018-04-29 18:36:32 -0500

    email, yapf

--- watch/setup.py
+++ watch/setup.py
@@ -1,12 +1,12 @@
-
 from distutils.core import setup
 
-setup(name="watch",
-      version="1.0",
-      maintainer="Skip Montanaro",
-      maintainer_email="skip@pobox.com",
-      description="Typing Watcher",
-      url="http://sourceforge.net/projects/watch",
-      py_modules=['collector'],
-      scripts=['watch.py', 'watch-server.py'],
-      )
+setup(
+    name="watch",
+    version="1.0",
+    maintainer="Skip Montanaro",
+    maintainer_email="skip.montanaro@gmail.com",
+    description="Typing Watcher",
+    url="http://sourceforge.net/projects/watch",
+    py_modules=['collector'],
+    scripts=['watch.py', 'watch-server.py'],
+)
