b7728892fd8cab3749586eef2d03081902fdf8f3
Skip Montanaro <skip.montanaro@gmail.com>
2018-04-29 18:37:34 -0500

    yapf, pylint

--- watch/collector.py
+++ watch/collector.py
@@ -1,6 +1,7 @@
 import time
 import sys
 
+
 class Collector:
     def __init__(self, debug=False):
         self.time = 0
@@ -8,13 +9,13 @@ class Collector:
         self.work = 20.0
         self.rest = 3.0
 
-    def now(self):
+    def now(self):              # pylint: disable=no-self-use
         return time.time()
 
     def get(self):
         now = self.now()
         if self.debug:
-            print("idle: %.1f" % (now-self.time), file=sys.stderr)
+            print("idle: %.1f" % (now - self.time), file=sys.stderr)
         return self.time, self.work, self.rest, now
 
     def tick(self):
